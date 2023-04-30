1fdb116d4045df30cfe7d6a69f1cade501c767a0
BetaDeltic <email@betadeltic.com>
2023-04-30 20:14:30 +0200

    Create SettingsCmdTests.cs

---
+++ YASudoku.Tests/ViewModels/GameViewModel/Commands/SettingsCmdTests.cs
@@ -0,0 +1,20 @@
+﻿namespace YASudoku.Tests.ViewModels.GameViewModel.Commands;
+
+public class SettingsCmdTests : GameCommandsTestsBase
+{
+    private void ClickSettings() => gameVM.OpenSettings();
+
+    private void AssertSettingsAreOpen() => Assert.True( VisualState.SettingsVS.AreSettingsVisible );
+
+    [Theory]
+    [MemberData( nameof( ArrangeActions ) )]
+    public void UnderAllCircumstances_ClickSettings_OpensSettings( Action arrangeAction )
+    {
+        // Arrange
+        arrangeAction();
+        // Act
+        ClickSettings();
+        // Assert
+        AssertSettingsAreOpen();
+    }
+}
